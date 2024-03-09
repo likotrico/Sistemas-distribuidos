484bf5dfd116a4605aa648772113da22b9a9e1e0
likotrico <112947493+likotrico@users.noreply.github.com>
2024-03-09 17:14:59 -0300

    Add files via upload

--- clientTrabalho.py
+++ clientTrabalho.py
@@ -1,16 +1,14 @@
 import socket
 import threading
+import time
 
 def receber_mensagens(sock):
     while (True):
         data = sock.recv(2048)
-        mensagem = data.decode()
-        if mensagem!='0':
-            #Imprimindo a mensagem recebida
-            print(data.decode())
-        else:
-            sock.close()
-            break
+        #mensagem = data.decode()
+        mensagem = list(data)
+        if(len(mensagem) != 0):
+            print(mensagem)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
@@ -20,13 +18,15 @@ print ("Conectando %s porta %s" % server_address)
 sock.connect(server_address)
 recepcao = threading.Thread(target=receber_mensagens,args=(sock,))
 recepcao.start()
-while (True):    
-    message = input("Envie qualquer mensagem para visualizar as horas.")
-    #Enviando mensagem ao servidor
-    sock.sendall(message.encode('utf-8'))
-    #message1 = sock.recv(2048)
-    #message1 = message1.decode()
+while (True): 
+
+    message = input("Adivinhe uma palavra com 5 letras")
+    
     if message=='0':
         break
 
+    #Enviando mensagem ao servidor
+    sock.sendall(message.encode('utf-8'))
+    
+    
 recepcao.join()
--- serverTrabalho.py
+++ serverTrabalho.py
@@ -2,61 +2,50 @@ import socket
 import threading
 from datetime import datetime
 
-"""def conexao_cliente(client,address):
-    
-    while (True):    
-        data = client.recv(2048)
-        
-        data_e_hora_atuais = datetime.now()
-        hora = data_e_hora_atuais.strftime('%H:%M:%S')
-        
-        mensagem = data.decode()
-        if (mensagem!='0'):
-            client.sendall(hora.encode())
-        else:
-            client.sendall('0'.encode())
-            break
-    #Fechando o socket
-    client.close()"""
-
 def verificarPalavra(client,address,choosen_Word):
     print('entrou')
     #decodificando dados    
     data = client.recv(2048)
     word = data.decode()
-    vetor = ""
+    
+    #client.send(bytes(vetor))
 
     if(len(word) == len(choosen_Word)): #verificando se possui mesmo tamanho
+
+        #cria o vetor no tamanho da palavra
+        vetor = []
+        for i in range(len(choosen_Word)):
+            vetor.append(0)
+        
         for i in range(len(choosen_Word)): #verificando se as posições batem
             if word[i] == choosen_Word[i]:
-                vetor = vetor + '1'
+                vetor[i] = 1
                 print('verde')
-                #print('colocar aqui o marcado verde')
             elif word[i] in choosen_Word:
-                vetor = vetor + '2'
+                vetor[i] = 2
                 print('laranja')
-        print('ok')
+        print('fim da verificacao')
     else:
-        vetor = vetor + '0'
-        print('deu pau!')
+        print('Palavras de tamanhos diferentes!')
 
-    client.sendall(vetor.encode())
+    client.sendall(bytes(vetor))
     client.close()
 
 
 
-sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-    
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_address = ('localhost', 20001)
 print ("Iniciando servidor na porta %s %s" % server_address)
+
 #Reservando porta
 sock.bind(server_address)
 #Escutando na porta reservada
 sock.listen(1)
 
 #Iniciando protocolo
+
 
 choosen_Word = 'comer'
 
